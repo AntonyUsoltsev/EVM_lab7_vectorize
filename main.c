2fe3ad79ece2a1ee6b41101abba56eae567847cc
Antony <>
2022-12-08 15:13:21 +0700

    some vector work

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
-
-#define N 4
-#define M 10
-
-void mult(float *B, float *A, float *R) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            float summ = 0;
-            for (int k = 0; k < N; k++) {
-                summ += B[i * N + k] * A[j * N + k];
+#include <xmmintrin.h>
+#include <time.h>
+#define N 8
+#define M 100000
+void mult(float *a, float *b, float *R) {
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            R[i * N + j] = 0;
+    __m128 *xx = (__m128 *) b;
+    __m128 *m128_result = (__m128 *) R;
+    __m128 mult, tmp;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            mult = _mm_set1_ps(a[i * N + j]);
+            for (int k = 0; k < N / 4; ++k) {
+                tmp = _mm_mul_ps(mult, xx[N * j / 4 + k]);
+                m128_result[N * i / 4 + k] = _mm_add_ps(m128_result[N * i / 4 + k], tmp);
             }
-            R[i * N + j] = summ;
         }
     }
 }
+//void mult(float *B, float *A, float *R) {
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < N; j++) {
+//            float summ = 0;
+//            for (int k = 0; k < N; k++) {
+//                summ += B[i * N + k] * A[k * N + j];
+//            }
+//            R[i * N + j] = summ;
+//        }
+//    }
+//}
 
 void summ(float *A, float *B, float *C) {
     for (int i = 0; i < N; i++) {
@@ -41,13 +58,15 @@ int main() {
     float *I = calloc(N * N, sizeof(float));
     float *B = calloc(N * N, sizeof(float));
     float *R = calloc(N * N, sizeof(float));
-
+    float *cur = calloc(N * N, sizeof(float));
+    float *cur_2 = calloc(N * N, sizeof(float));
+    clock_t beg = clock();
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (i == j)
                 A[i * N + j] = 4.0f;
             else
-                A[i * N + j] = 5.0f;
+                A[i * N + j] = 3.0f;
         }
     }
     for (int i = 0; i < N; i++) {
@@ -88,22 +107,17 @@ int main() {
     //print_matr(B);
 
     mult(B, A, R);
+
     //print_matr(B);
-    // print_matr(R);
+     //print_matr(R);
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            R[i * N + j] = I[j * N + i] - R[i * N + j];
+            R[i * N + j] = I[i * N + j] - R[i * N + j];
         }
     }
     // print_matr(R);
 
-    float *cur = calloc(N * N, sizeof(float));
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++) {
-//            cur[i * N + j] = R[i * N + j];
-//        }
-//    }
-    float *cur_2 = calloc(N * N, sizeof(float));
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cur_2[i * N + j] = R[i * N + j];
@@ -120,6 +134,11 @@ int main() {
 
     }
     mult(I, B, A_1);
-    print_matr(A_1);
+   // print_matr(A_1);
+    mult(A,A_1,cur);
+    //print_matr(cur);
+    clock_t end = clock();
+    double time_used = ((double) (end - beg)) / CLOCKS_PER_SEC;
+    printf("%lf\n", time_used);
     return 0;
 }
